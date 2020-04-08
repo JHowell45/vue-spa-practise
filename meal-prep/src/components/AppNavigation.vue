534f35a40e73e48d5bb0142486fb4d1acdd5ff86
JHowell45 <jhowell45@googlemail.com>
2020-04-08 16:07:17 +0100

    Fixing disparaging issues between the outdated code and modern package.

--- meal-prep/src/components/AppNavigation.vue
+++ meal-prep/src/components/AppNavigation.vue
@@ -1,22 +1,22 @@
 <template>
   <v-app-bar app color="brown darken-4" dark>
-    <!-- <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
     <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
-    <v-btn flat>Menu</v-btn>
+    <v-btn text>Menu</v-btn>
     <v-spacer></v-spacer>
-    <v-btn flat>SIGN IN</v-btn>
-    <v-btn color="brown lighten-3">JOIN</v-btn> -->
+    <v-btn text>SIGN IN</v-btn>
+    <v-btn color="brown lighten-3">JOIN</v-btn>
 
-    <v-app-bar-side-icon
+    <!-- <v-app-bar-nav-icon
       class="hidden-md-and-up"
       @click="drawer = !drawer"
-    ></v-app-bar-side-icon>
+    ></v-app-bar-nav-icon>
     <v-spacer class="hidden-md-and-up"></v-spacer>
-    <v-app-bar-title>{{ appTitle }}</v-app-bar-title>
-    <v-btn flat class="hidden-sm-and-down">Menu</v-btn>
+    <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+    <v-btn text class="hidden-sm-and-down">Menu</v-btn>
     <v-spacer class="hidden-sm-and-down"></v-spacer>
-    <v-btn flat class="hidden-sm-and-down">SIGN IN</v-btn>
-    <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
+    <v-btn text class="hidden-sm-and-down">SIGN IN</v-btn>
+    <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn> -->
   </v-app-bar>
 </template>
 
