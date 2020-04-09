c30e0dfe9744822fd9b2864bf0b024ddae42b997
JHowell45 <jhowell45@googlemail.com>
2020-04-09 15:26:28 +0100

    Updated menu bar to resize with window.

--- meal-prep/src/components/AppNavigation.vue
+++ meal-prep/src/components/AppNavigation.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app-bar app color="brown darken-4" dark>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon>
     <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
     <v-btn text>Menu</v-btn>
     <v-spacer></v-spacer>
     <v-btn text>SIGN IN</v-btn>
-    <v-btn color="brown lighten-3">JOIN</v-btn>
+    <v-btn color="brown lighten-3">JOIN</v-btn> -->
 
-    <!-- <v-app-bar-nav-icon
+    <v-app-bar-nav-icon
       class="hidden-md-and-up"
       @click="drawer = !drawer"
     ></v-app-bar-nav-icon>
@@ -16,7 +16,7 @@
     <v-btn text class="hidden-sm-and-down">Menu</v-btn>
     <v-spacer class="hidden-sm-and-down"></v-spacer>
     <v-btn text class="hidden-sm-and-down">SIGN IN</v-btn>
-    <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn> -->
+    <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
   </v-app-bar>
 </template>
 
@@ -26,8 +26,9 @@ export default {
   data() {
     return {
       appTitle: "Meal Prep",
+      drawer: false
     };
-  },
+  }
 };
 </script>
 
